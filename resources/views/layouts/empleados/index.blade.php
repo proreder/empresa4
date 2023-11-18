9273efcbb16107e3767ab2486fbdfd1f9459f506
proreder <proreder@users.noreply.github.com>
2023-11-18 20:22:48 +0100

    modificacion index
    modificación tabla index, se añade a la tabla la propiedad resposive

--- resources/views/layouts/empleados/index.blade.php
+++ resources/views/layouts/empleados/index.blade.php
@@ -76,15 +76,19 @@
 @stop
 
 @section('css')
-  
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap4.min.css">
 @stop
 
 @section('js')
     <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-   
+    <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap4.min.js"></script>
+    <script>
         $(document).ready(function(){
             $('#empleados').DataTable({
+                responsive : true,
             "language" : {
                 "search" :          "Buscar",
                 "lengthMenu" :      "Mostrar _MENU_ registros por página",
