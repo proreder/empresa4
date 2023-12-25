1076b5986f94a7441f03f527b45a02cea4278543
proreder <proreder@hotmail.com>
2023-12-26 00:17:54 +0100

    continuacion del modulo conductos

--- app/Http/Controllers/ConductoresController.php
+++ app/Http/Controllers/ConductoresController.php
@@ -10,11 +10,10 @@ class ConductoresController extends Controller
 
     public function index(){
         //obtenemos todos los registros de la tabla Empleado
-        $datos['conductores']=ConductoresModel::all();
-        echo('hola');
-        print_r($conductores);
-        dd($conductores);
-        return view('layouts.conductores.index', $datos);
+        //$datos['conductores']=ConductoresModel::all();
+        //return view('layouts.conductores.index', $datos);
+        $datos=ConductoresModel::paginate(10);
+        return view('layouts.conductores.index', compact($datos));
      
     }
     //obtenemos un solo registro de la tabla conductores
@@ -26,6 +25,27 @@ class ConductoresController extends Controller
      
     }
 
-    
+    //metodo para guardar los datos en la base de datos
+    public function store(Request $request){
+        
+    }
+
+    //obtenemos los conductores de la base de datos
+    public function listarConductores(Request $request){
+        //si hay respuesta ajax correcta la procesamos
+        if($request->ajax()){
+            return response()->json([
+                'code'      => 200,
+                'msg'       => 'success',
+            ],404);           
+        }else{
+            //si hay un error devolvemos un json con error 404 y el texto de error
+            return response()->json([
+                'code'      => 404,
+                'msg'       => 'ERROR',
+                'message'   => 'Error, no se puede acceder a la página'
+            ],404);
+        }
+    }
     
 }
--- resources/views/layouts/conductores/index.blade.php
+++ resources/views/layouts/conductores/index.blade.php
@@ -17,12 +17,12 @@
         <p>Hola Usuario</p>
     @endrole
     <div class="container-flex">
-      
-       
-        
+
+
+
         <a href="{{ url('conductores/create') }}" class="btn btn-success">Alta de conductor</a>
         <br><br>
-        
+        <div  id="spinner"></div>
         <table id="conductores" class="table table-striped border">
             <thead class="thead-light">
                 <tr>
@@ -41,41 +41,73 @@
                 @forelse ($conductores as $conductor)
                     <tr>
                         <td><img src="data:image/png;base64,
-                            <?php 
-                                 echo base64_encode($conductor->empleado->foto); 
-                            ?>"  alt="" width="40">
+                            <?php
+                            echo base64_encode($conductor->empleado->foto);
+                            ?>" alt=""
+                                width="40">
                         </td>
-                        
-                        <td>{{ $conductor->nifnie_empleado}}</td>
-                        <td>{{ $conductor->permisos}}</td>
-                        <td>{{ ($conductor->cap = true ? 'Si' :  'No')}}</td>
+
+                        <td>{{ $conductor->nifnie_empleado }}</td>
+                        <td>{{ $conductor->permisos }}</td>
+                        <td>{{ $conductor->cap = true ? 'Si' : 'No' }}</td>
                         <td>{{ $conductor->tarjeta_tacografo = true ? 'Si' : 'No' }}</td>
-                        <td>{{ $conductor->tipo_ADR}}</td>
+                        <td>{{ $conductor->tipo_ADR }}</td>
                         <td>
-                            <form id="btnEliminar" action="{{url('/conductores/'.$conductor->id)}}" method="post">
-                                {{method_field('EDIT')}}
-                                <a href="{{url('/conductores/'.$conductor->id.'/edit/')}}" class="btn btn-primary btn-sm py-0">Editar</a> 
+                            <form id="btnEliminar" action="{{ url('/conductores/' . $conductor->id) }}" method="post">
+                                {{ method_field('EDIT') }}
+                                <a href="{{ url('/conductores/' . $conductor->id . '/edit/') }}"
+                                    class="text-success mx-1 editIcon" data-bs-toggle="modal"
+                                    data-bs-target="#editarConductor"><i class="bi-pencil-square h4"></i></a>
+
+                                <!-- Button trigger modal -->
+                                <!--<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal"></button>-->
                                 @csrf
-                                {{method_field('DELETE')}}
-                             <!--   <input type="submit" onclick="return confirm('¿Quieres borrar?')" id="btnEliminar" value="Borrar" class="btn btn-danger btn-sm py-0"> -->
-                                <button type="submit" value="Borrar" class="btn btn-danger btn-sm py-0">Borrar</button> 
-                            </form>   
+                                {{ method_field('DELETE') }}
+                                <!--   <input type="submit" onclick="return confirm('¿Quieres borrar?')" id="btnEliminar" value="Borrar" class="btn btn-danger btn-sm py-0"> -->
+                                <!--   <button type="submit" value="Borrar" class="text-danger mx-1 delete-Icon"><i class="bi-trash h5"></i></button> -->
+                                <a href="{{ url('/conductores/' . $conductor->id . '/delete/') }}"
+                                    class="text-danger mx-1 delete-Icon"><i class="bi-trash h4"></i></a>
+                            </form>
                         </td>
                     </tr>
                 @empty
-                    
                 @endforelse
             </tbody>
         </table>
-      <!--End card-->
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Launch demo modal
+        </button>
+        <!--End card-->
+
+
+        {{-- Modal --}}
+        <div class="modal fade" id="editarConductor" tabindex="-1" aria-labelledby="editarConductorLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h2 class="modal-title fs-5" id="editarConductorLabel">Editar un conductor</h2>
+                        <button type="button" class="btn btn-close" data-bs-dismiss="modal" aria-label="Close"><i class="bi bi-x-square h-3"></i></button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+                        <button type="button" class="btn btn-primary">Guardar cambios</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @stop
 
 @section('css')
-    <link rel="stylesheet" href=".public/vendor/adminlte/dist/css/adminlte.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="../public/css/sweetalert2.min.css">
 @stop
 
@@ -84,26 +116,61 @@
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>
     <script src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
     <script src="../public/build/assets/sweetalert2.all.min.js"></script>
     <script>
-        $(document).ready(function(){
-            $('#conductores').DataTable({
-                responsive : true,
-            "language" : {
-                "search" :          "Buscar",
-                "lengthMenu" :      "Mostrar _MENU_ registros por página",
-                "info"  :           "Página _PAGE_ de _PAGES_",
-                "zeroRecords" :    "No hay registros",
-                "infoEmpty" :     "",
-                "paginate" :        {
-                                        "previous"  :  "Anterior",
-                                        "next":       "Siguiente",
-                                        "first":       "Primero",
-                                        "last":        "último"
-                }   
-            }  
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+        });
+    </script>
+    
+    <script>
+        $(document).ready(function() {
+            $.ajax({
+                type: 'POST',
+                url:  '{{ route('conductores.index') }}',
+                dataType: 'json',
+                //antes de enviar la petición colocamos un spinner
+                beforeSend: function(){
+                    $('#spinner').html("<div class='cargando'><i class='fa-solid fa-spinner fa-5x'></i></div>");
+                },
+                //si hay error lo procesamos
+                error: function(data){
+                    //guardo en una variable el error del tipo que es
+                    let errorJSON=JSON.parse(data.responseText);
+                    Swal.fire({
+                        icon: "error",
+                        title: "Error de conexión",
+                        text: errorJSON.message,
+                         
+                    });        
+                },
+                //si no hay error los mostramos en la tabla
+                success: function(data){
+
+                }
             });
-            $('#btnEliminar').on('submit', function(e){
+            $('#conductores').DataTable({
+                responsive: true,
+                "language": {
+                    "search": "Buscar",
+                    "lengthMenu": "Mostrar _MENU_ registros por página",
+                    "info": "Página _PAGE_ de _PAGES_",
+                    "zeroRecords": "No hay registros",
+                    "infoEmpty": "",
+                    "paginate": {
+                        "previous": "Anterior",
+                        "next": "Siguiente",
+                        "first": "Primero",
+                        "last": "último"
+                    }
+                }
+            });
+            $('#btnEliminar').on('submit', function(e) {
                 e.preventDefault();
                 Swal.fire({
                     title: "¿Estás seguro?",
@@ -119,7 +186,7 @@
             })
         });
     </script>
-    @if(Session::has('success'))
+    @if (Session::has('success'))
         <script>
             Swal.fire({
                 title: "Borrado",
